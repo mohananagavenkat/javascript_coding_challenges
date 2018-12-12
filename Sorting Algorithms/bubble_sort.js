bf5f41fbe9f3f89cd398e0a98e3afda96b85e581
mohananagavenkat <mohananagavenkat@gmail.com>
2018-12-12 21:29:03 +0530

    bubble sort with flag

--- Sorting Algorithms/bubble_sort.js
+++ Sorting Algorithms/bubble_sort.js
@@ -9,12 +9,29 @@ function bubbleSort(a){
 	}
 	console.log(a);
 }
+// bubble sorting with flag variable
+function bubbleSort2(a) {
+	const length = a.length;
+	for (let i = 0; i < length - 1; i++) {
+		let flag = false;
+		for (let j = 0; j < length - 1 - i; j++) {
+			if (a[j] > a[j + 1]) {
+				swap(a, j, j + 1);
+				let flag = true;
+			}
+		}
+		if(flag == false){
+			break;
+		}
+	}
+	console.log(a);
+}
 
 function swap(a, i, j) {
     [a[i], a[j]] = [a[j], a[i]];
     return;
 }
-let unsorted = [2, 4, 1, 5, 3, 7, 2, 4, 1, 5, 3, 7, 2, 4, 1, 5, 3, 7, 2, 4, 1, 5, 3, 7, 2, 4, 1, 5, 3, 7, 2, 4, 1, 5, 3, 7, 2, 4, 1, 5, 3, 7, 2, 4, 1, 5, 3, 7];
+let unsorted = [30, 40, 10, 20, 15, 25, 65, 35];
 console.time()
 bubbleSort(unsorted);
 console.timeEnd();
